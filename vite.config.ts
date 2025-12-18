60991a6a8a2d47d48e6085166b0bfa06f0d3d2de
Dan Zhou <danzho@proton.me>
2025-12-18 11:23:29 -0600

    experiment with codegen alias for sdk

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,18 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@moneypot/experience-react-sdk/generated": "./src/__generated__",
+    },
+  },
   define: {
     // Fallback if .env is missing (StackBlitz filters .env files)
     ...(process.env.VITE_GRAPHQL_URL
       ? {}
       : {
           "import.meta.env.VITE_GRAPHQL_URL": JSON.stringify(
-            "https://hub1.moneypot.com/graphql"
+            "https://hub1.moneypot.com/graphql",
           ),
         }),
   },
